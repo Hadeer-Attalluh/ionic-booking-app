ae07c239e1ce6f924be0d11c05d766b04feda541
Hadeer Bayumi <hadeer.bayumi.ext@orange.com>
2021-02-15 15:52:52 +0200

    view discover list

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../_models/place';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  places: Place[];
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.placesService.getDiscoverPlaces().subscribe(
+      places => this.places = places
+    )
   }
 
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Place } from './_models/place';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private places = [
+    new Place({
+      id: '2',
+      title: 'place 2',
+      description: 'xyz 2',
+      price: 120,
+      imageUrl: "https://www.lindsaysilberman.com/wp-content/uploads/2020/02/The_10_Most_Instagrammable_Places_in_London-09.jpg"
+    }),
+    new Place({
+      id: '1',
+      title: 'place 1',
+      description: 'xyz 1',
+      price: 100
+    }),
+    new Place({
+      id: '3',
+      title: 'place 3',
+      description: 'xyz 3',
+      price: 300,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1D-FtE5EQrPPgl54LyOZj82XdRHggArZJLg&usqp=CAU"
+    }),
+  ]
+  constructor() { }
+
+  getDiscoverPlaces(): Observable<Place[]> {
+    return of([...this.places]);
+  }
+}
